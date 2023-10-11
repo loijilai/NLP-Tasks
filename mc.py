c1b2e31e8e2dddd18ffa8b859e23206d36616a07
Cheng Lin Lai <loijilai@cml5.csie.ntu.edu.tw>
2023-10-11 21:40:15 +0800

    Edit files

--- mc.py
+++ mc.py
@@ -5,12 +5,23 @@ from transformers import AutoConfig, AutoTokenizer, AutoModelForMultipleChoice, 
 # for data preprocessing
 from datasets import load_dataset
 import json
+# for data collator
+from dataclasses import dataclass
+from typing import Optional, Union
+from transformers.tokenization_utils_base import PreTrainedTokenizerBase, PaddingStrategy
 
-def preprocess_function(examples:dict):
-    # examples = train_set[0]
-    # examples.keys() is dict_keys(['paragraphs', 'answer', 'question', 'relevant', 'id'])
+def preprocess_function(examples):
+    # [[1, 1, 1, 1], [2, 2, 2, 2], ...]
+    """
+    first_sentences[0] = 
+    ['舍本和誰的數據能推算出連星的恆星的質量？',
+    '舍本和誰的數據能推算出連星的恆星的質量？',
+    '舍本和誰的數據能推算出連星的恆星的質量？',
+    '舍本和誰的數據能推算出連星的恆星的質量？']
+    """
     first_sentences = [[context] * 4 for context in examples['question']]
 
+    # [[p1, p2, p3, p4], [p1, p2, p3, p4], ...]
     second_sentences = [
         [context_json[idx] for idx in related_p_idx]
         for related_p_idx in examples['paragraphs']
@@ -30,24 +41,67 @@ def preprocess_function(examples:dict):
     )
 
     # Unflatten and group by four
-    tokenized_inputs = {
-        k: [v[i:i + 4] for i in range(0, len(v), 4)]
-        for k, v in tokenized_examples.items()
-    }
+    tokenized_result = {}
+    for k, v in tokenized_examples.items():
+        # tokenized_examples.keys() = input_ids, token_type_ids, attention_mask
+        tokenized_result[k] = [v[i:i + 4] for i in range(0, len(v), 4)]
 
-    # train or valid set
+    # train and valid set
     if 'relevant' in examples.keys():
-        tokenized_inputs['label'] = [
+        tokenized_result['label'] = [
             para.index(p_uid)
             for para, p_uid in zip(examples['paragraphs'], examples['relevant'])
         ]
-    else:
-        tokenized_inputs['label'] = [0] * len(examples['paragraphs'])
+    else: # test set, initialize with 0
+        tokenized_result['label'] = [0] * len(examples['paragraphs'])
 
-    return tokenized_inputs
+    return tokenized_result
+
+# Data collator transforms dataset into a batch of tensors
+@dataclass
+class DataCollatorForMultipleChoice:
+    """
+    Data collator that will dynamically pad the inputs for multiple choice received.
+    """
+
+    tokenizer: PreTrainedTokenizerBase
+    padding: Union[bool, str, PaddingStrategy] = True
+    max_length: Optional[int] = None
+    pad_to_multiple_of: Optional[int] = None
+
+    def __call__(self, features):
+        label_name = "label" if "label" in features[0].keys() else "labels"
+        labels = [feature.pop(label_name) for feature in features]
+        batch_size = len(features)
+        num_choices = len(features[0]["input_ids"])
+        flattened_features = [
+            [{k: v[i] for k, v in feature.items()} for i in range(num_choices)] for feature in features
+        ]
+        flattened_features = sum(flattened_features, [])
+
+        batch = self.tokenizer.pad(
+            flattened_features,
+            padding=self.padding,
+            max_length=self.max_length,
+            pad_to_multiple_of=self.pad_to_multiple_of,
+            return_tensors="pt",
+        )
+
+        batch = {k: v.view(batch_size, num_choices, -1) for k, v in batch.items()}
+        batch["labels"] = torch.tensor(labels, dtype=torch.int64)
+        return batch
+
+# Metric
+def compute_metrics(eval_predictions):
+    predictions, label_ids = eval_predictions
+    preds = np.argmax(predictions, axis=1)
+    return {"accuracy": (preds == label_ids).astype(np.float32).mean().item()}
+
 
 datafiles = {"train": "/home/loijilai/CS-hub/DL/adl/paragraph-selection-QA/dataset/train.json",
              "valid": "/home/loijilai/CS-hub/DL/adl/paragraph-selection-QA/dataset/valid.json"}
+# datafiles = {"train": "/tmp2/loijilai/adl/hw1/dataset/train.json",
+#              "valid": "/tmp2/loijilai/adl/hw1/dataset/valid.json"}
 raw_dataset = load_dataset("json", data_files=datafiles)
 # raw_dataset['train][0]['question']  = '舍本和誰的數據能推算出連星的恆星的質量？'
 
@@ -59,9 +113,7 @@ tokenizer = AutoTokenizer.from_pretrained("bert-base-chinese")
 # tokenizer.model_max_length = 512
 # tokenizer.model_input_names = ['input_ids', 'token_type_ids', 'attention_mask']
 
-tmp = train_set[0]
-preprocess_function(tmp)
-## train_set = train_set.map(preprocess_function, batched=True)
+train_set = train_set.map(preprocess_function, batched=True)
 # More keys: input_ids, token_type_ids, attention_mask, label
 # Dataset({
 #     features: ['paragraphs', 'answer', 'question', 'relevant', 'id', 'input_ids', 'token_type_ids', 'attention_mask', 'label'],
@@ -82,9 +134,38 @@ preprocess_function(tmp)
 # (a list of four elements)
 # train_set[0]['label'] = 3
 
+valid_set = raw_dataset['valid']
+valid_set = valid_set.map(preprocess_function, batched=True)
+
 
 "----------------------------------------------------------------------------------------"
 model = AutoModelForMultipleChoice.from_pretrained("bert-base-chinese")
 
+training_args = TrainingArguments(
+    output_dir="outputs",
+    evaluation_strategy="epoch",
+    save_strategy="epoch",
+    load_best_model_at_end=True,
+    learning_rate=5e-5,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    num_train_epochs=1,
+    weight_decay=0.01,
+    push_to_hub=False,
+    fp16=True,
+)
 
+# Initialize our Trainer
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset = train_set,
+    eval_dataset = valid_set,
+    tokenizer=tokenizer,
+    data_collator=DataCollatorForMultipleChoice(tokenizer),
+    compute_metrics=compute_metrics,
+)
+
+# Training
+trainer.train()
 ""
