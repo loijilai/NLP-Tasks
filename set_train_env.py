04d5a94d40ea34a233a697b695aba1efc6d6e5d0
loijilai-cml <loijilai@gmail.com>
2023-10-20 01:15:35 +0800

    Readme instruction tested

--- set_train_env.py
+++ set_train_env.py
@@ -1,8 +1,17 @@
 import os
 import gdown
-os.mkdir("outputs")
-os.mkdir("outputs/mc")
-os.mkdir("outputs/qa")
-os.mkdir("outputs/result")
-dataset_url = "https://drive.google.com/drive/folders/1MoWExudAkSWXwibTrLPIaCaN6JScRaTo?usp=share_link"
-gdown.download_folder(dataset_url)
+
+os.mkdir("model")
+# For storing mc model checkpoints
+os.mkdir("model/mc")
+# For storing qa model checkpoints
+os.mkdir("model/qa")
+
+# Download the dataset only if you don't have it already
+if not os.path.exists("./dataset"):
+    print("Downloading dataset...")
+    dataset_url = "https://drive.google.com/drive/folders/1MoWExudAkSWXwibTrLPIaCaN6JScRaTo?usp=share_link"
+    gdown.download_folder(dataset_url)
+    print("Dataset downloaded!")
+else:
+    print("Dataset already exists!")
